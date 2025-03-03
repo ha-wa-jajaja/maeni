8d239aa0341de023e7f596853ee83fcd740fa574
ha-wa-jajaja <wtfuhell@gmail.com>
2025-03-03 16:17:09 +0800

    update type

--- frontend/src/type/type.ts
+++ frontend/src/type/type.ts
@@ -3,24 +3,21 @@ type Lang = "ja";
 type ItemBaseType = "vocab" | "grammar";
 
 type ItemBase<T extends ItemBaseType> = {
+    id: number;
     type: T;
     content: string;
     lang: Lang;
     translation: string;
     description: string;
-    examples: string[];
     correctCount: number;
     wrongCount: number;
 };
 
-export type VocabItem = ItemBase<"vocab"> & {
-    id: number;
-};
-
-export type GrammarItem = {
+export type Example = {
     id: number;
     content: string;
     lang: Lang;
     translation: string;
-    points: ItemBase<"grammar">[];
+    description: string | null;
+    points: Array<ItemBase<"vocab" | "grammar">>;
 };
